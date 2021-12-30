82b01f5e73e7520e0d43cfe75b476c607b585be1
jcgo <jeancgo@pm.me>
2021-12-30 03:09:34 -0400

    docs: add reference link for `window` function in `day 1 - Sonar Sweep - part 2.py`

--- day 1 - Sonar Sweep - part 2.py
+++ day 1 - Sonar Sweep - part 2.py
@@ -1,12 +1,14 @@
 from itertools import islice
 
+
 def window(seq, n=2):
+    # taken from https://stackoverflow.com/a/6822773 and https://docs.python.org/release/2.3.5/lib/itertools-example.html
     it = iter(seq)
     res = tuple(islice(it, n))
-    
+
     if len(res) == n:
         yield res
-    
+
     for elem in it:
         res = res[1:] + (elem,)
         yield res
