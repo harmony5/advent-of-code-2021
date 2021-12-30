3f28a753de3fd9003dc9037b70ef4ae523a83324
jcgo <jeancgo@pm.me>
2021-12-30 03:32:48 -0400

    fix: add int casting `day 1 - Sonar Sweep - part 2.py`
    Lines needed to be converted to integers for comparison.

--- day 1 - Sonar Sweep - part 2.py
+++ day 1 - Sonar Sweep - part 2.py
@@ -4,7 +4,7 @@ from operator import lt
 # solution inspired by https://mathspp.com/blog/advent-of-code-sonar-sweep-analysis
 
 with open("input.txt") as f:
-    data = (line for line in f.readlines())
+    data = (int(line) for line in f.readlines())
 
 curs, nexts = tee(data)
 nexts = islice(nexts, 3, None)
