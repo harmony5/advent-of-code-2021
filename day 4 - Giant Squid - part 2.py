6fa447bb99dbf7971a58b54e3389641cf3cd8cbb
jcgo <jeancgo@pm.me>
2022-01-08 20:50:07 -0400

    refactor: add a `Board` class to better model the problem
    This helps to simplify the problem modeling and solving.

--- day 4 - Giant Squid - part 2.py
+++ day 4 - Giant Squid - part 2.py
@@ -1,66 +1,82 @@
-from itertools import chain, tee
-from pprint import pprint
-from re import split as re_split
+from itertools import chain
+
+flatten = chain.from_iterable
+
+
+class Board:
+    """Class defining a 2D bingo board"""
+
+    def __init__(self, rows):
+        self.__rows = rows
+
+    @property
+    def is_winner(self):
+        """A board wins if any of its rows or columns is empty"""
+        return any(
+            self.is_empty_row(i) or self.is_empty_col(i) for i in range(len(self))
+        )
+
+    def __getitem__(self, index):
+        return self.__rows[index]
+
+    def __len__(self):
+        return len(self.__rows)
+
+    def __str__(self):
+        return "\n".join(" ".join(str(col).rjust(3) for col in row) for row in self)
+
+    def __repr__(self):
+        return f"Board({self.__rows})"
+
+    def remove_draw(self, draw):
+        """Remove a draw from the board"""
+        for row in self:
+            for j, col in enumerate(row):
+                if col == draw:
+                    row[j] = 0
+
+    def is_empty_row(self, row):
+        """Check if the row is empty"""
+        return not any(self[row])
+
+    def is_empty_col(self, col):
+        """Check if the column is empty"""
+        return not any(row[col] for row in self)
 
 
 def prepare(data):
-    draws, *boards = re_split(r"(?m)^$", data.strip())
+    draws, *boards = data.strip().split("\n\n")
     draws = draws.strip().split(",")
     boards = [
-        [line.strip().split() for line in board.strip().split("\n")] for board in boards
+        Board([line.strip().split() for line in board.strip().split("\n")])
+        for board in boards
     ]
     return draws, boards
 
 
-def remove_draw(board, d):
-    removed_coords = []
-    for i, row in enumerate(board):
-        for j, num in enumerate(row):
-            if num == d:
-                row[j] = None
-                removed_coords.append((i, j))
-
-    return removed_coords
-
-
-def empty_row_or_col(board, x, y):
-    return not any(board[x]) or not any(row[y] for row in board)
-
-
 def sum_board(board):
-    return sum(int(n) for n in chain.from_iterable(board) if n is not None)
+    return sum(map(int, flatten(board)))
 
 
-def solve():
+if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read()
+
     draws, boards = prepare(data)
 
     # board, draw * board sum
     last_winning_board = ([], 0)
-    for i, draw in enumerate(draws):
-        # remove boards that have already won
-        filtered_boards = [
-            b
-            for b in boards
-            if not any(empty_row_or_col(b, x, y) for x, y in zip(*tee(range(5))))
-        ]
-        if not filtered_boards:
+    for draw in draws:
+        remaining_boards = [b for b in boards if not b.is_winner]
+
+        if not remaining_boards:
             break
 
-        for board in filtered_boards:
-            removed_coords = remove_draw(board, draw)
+        for board in remaining_boards:
+            board.remove_draw(draw)
 
-            if i >= 4:
-                # check if the current board wins this round with the current draw
-                if removed_coords and any(
-                    empty_row_or_col(board, x, y) for x, y in removed_coords
-                ):
-                    last_winning_board = board, int(draw) * sum_board(board)
+            if board.is_winner:
+                last_winning_board = board, int(draw) * sum_board(board)
 
-    pprint(last_winning_board[0])
+    print(last_winning_board[0])
     print(last_winning_board[1])
-
-
-if __name__ == "__main__":
-    solve()
